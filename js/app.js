279126531652dd69359bfb792a161bc8a8666698
miladtaghizadeh1382 <miladtaghizadeh1382@outlook.com>
2024-01-15 14:35:36 +0330

    feat:index added and upgraded and tailwind config upgraded

--- js/app.js
+++ js/app.js
@@ -26,3 +26,15 @@ function signup() {
 
     console.log("Sign Up:", username, password, email, phone);
 }
+// index page
+function showSection(sectionNumber) {
+    // Hide all sections
+    const sections = document.querySelectorAll('[id^="section"]');
+    sections.forEach(section => {
+        section.classList.add('hidden');
+    });
+
+    // Show the selected section
+    const selectedSection = document.getElementById(`section${sectionNumber}`);
+    selectedSection.classList.remove('hidden');
+}
