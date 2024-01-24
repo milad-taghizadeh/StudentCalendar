d39037718eb470c44d12f0e1abf1ebb3bc522af7
hossein jahandideh <h.jahandideh2@gmail.com>
2024-01-24 17:09:47 +0330

    FIX : adding cors

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const dotenv = require("dotenv");
 dotenv.config();
 
 const cookieParser = require("cookie-parser");
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
