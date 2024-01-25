f11fe299b1f0c1ad23728142ea8db3ec8a28d3e4
miladtaghizadeh1382 <miladtaghizadeh1382@outlook.com>
2024-01-25 12:48:19 +0330

    fix:index upgraded

--- js/app.js
+++ js/app.js
@@ -32,3 +32,23 @@ fetch('http://localhost:3000/api/user/', {
         document.getElementById('email').innerHTML = `<span>آدرس ایمیل : </span><span>${data.email}</span>`
         document.getElementById('phonenumber').innerHTML = `<span>شماره تماس : </span><span>${data.phonenumber}</span>`
     })
+    .catch(error => console.error(error))
+
+// ADD EVENT 
+let eventName = document.getElementById('evname').value;
+let eventSub = document.getElementById('evsub').value;
+let eventDate = document.getElementById('evdate').value;
+let eventCaption = document.getElementById('evcaption').value;
+const sendEventReq = {
+    title: eventName,
+    subject: eventSub,
+    describtion: eventCaption,
+    date: eventDate
+}
+fetch('localhost:3000/api/event/', {
+    credentials: 'include',
+    method: 'POST',
+    headers: {
+        'content-type': 'application/json'
+    }
+})
