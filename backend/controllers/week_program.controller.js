99e5dc16cd6201f2b14656c56f74fb6edbb30d2b
hossein jahandideh <h.jahandideh2@gmail.com>
2024-01-28 12:37:56 +0330

    FIX: update week program fixed.

--- backend/controllers/week_program.controller.js
+++ backend/controllers/week_program.controller.js
@@ -20,16 +20,65 @@ const createProgram = async (req, res) => {
 
 const updateProgram = async (req, res) => {
   try {
-    const updatedProgram = await weekProgram.findOneAndUpdate(
+    const updatedProgram = await weekProgram.findOne({
+      user_id: req.user.id,
+      isOddWeek: req.query.isOddWeek,
+    });
+    const keys = Object.keys(req.body.week);
+
+    keys.forEach((key) => {
+      switch (key) {
+        case "sat":
+          updatedProgram._doc.week.sat = updatedProgram.week.sat.concat(
+            req.body.week.sat
+          );
+          break;
+        case "sun":
+          updatedProgram._doc.week.sun = updatedProgram.week.sun.concat(
+            req.body.week.sun
+          );
+          break;
+        case "mon":
+          updatedProgram._doc.week.mon = updatedProgram.week.mon.concat(
+            req.body.week.mon
+          );
+          break;
+        case "tue":
+          updatedProgram._doc.week.tue = updatedProgram.week.tue.concat(
+            req.body.week.tue
+          );
+          break;
+        case "wed":
+          updatedProgram._doc.week.wed = updatedProgram.week.wed.concat(
+            req.body.week.wed
+          );
+          break;
+        case "thu":
+          updatedProgram._doc.week.thu = updatedProgram.week.thu.concat(
+            req.body.week.thu
+          );
+          break;
+        case "fri":
+          updatedProgram._doc.week.fri = updatedProgram.week.fri.concat(
+            req.body.week.fri
+          );
+          break;
+
+        default:
+          break;
+      }
+    });
+
+    const result = await weekProgram.updateOne(
       {
         user_id: req.user.id,
         isOddWeek: req.query.isOddWeek,
       },
-      { $set: req.body },
+      updatedProgram,
       { new: true }
     );
 
-    res.status(200).json(updatedProgram);
+    res.status(200).json(result);
   } catch (err) {
     res.status(400).json(err);
     console.log(err);
